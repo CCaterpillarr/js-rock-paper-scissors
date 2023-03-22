91c5d2d22fc2e8fd61aaf1198851607f8994bb10
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-22 12:17:48 +0100

    Add function for spellcheck

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 function getComputerChoice(){
-    let computerRoll = Math.random() * 100;
+    let computerRoll = Math.random() * 100 + 1;
     let computerChoice;
-    if (computerRoll < 33){
+    if (computerRoll <= 33){
         computerChoice = "Rock";
-    } else if (computerRoll < 66){
+    } else if (computerRoll <= 66){
         computerChoice = "Paper";
     } else {
         computerChoice = "Scissors";
@@ -11,4 +11,19 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+function compareString(string){
+    string = string.toLowerCase();
+    if (string === "rock" || string === "paper" || string === "scissors") {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+let playerChoice = prompt("Your choice:");
+
+console.log(compareString(playerChoice));
+
+
+console.log(playerChoice);
 console.log(getComputerChoice());

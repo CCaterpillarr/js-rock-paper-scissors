6e71bdc4e54e08f5de6ca76cf0f990a2544c0bf7
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-27 15:13:21 +0200

    Remove option buttons once game is done

--- javascript.js
+++ javascript.js
@@ -21,11 +21,6 @@ function compareString(string){
     }
 }
 
-function rockClick(){
-    let choice = "rock";
-    return choice;
-}
-
 function gameRound(playerChoice){
 
     let computerChoice = getComputerChoice();
@@ -51,10 +46,7 @@ function gameRound(playerChoice){
         round++;
     }
 
-    if (round < 5){
-        // Display round results at bottom
-        // Count score at bottom
-    } else {
+    if (round === 5){
         gameEnd();
     }
 }
@@ -62,29 +54,29 @@ function gameRound(playerChoice){
 function gameEnd(){
     console.log(`Your score: ${score}/5`);
     if (score >= 3){
-        resultsPanel.textContent = `Your score: ${score}/5 - You won!`;
+        resultsPanel.textContent += ` Your score: ${score}/5 - You won!`;
     } else {
-        resultsPanel.textContent = `Your score: ${score}/5 - You lost!`;
+        resultsPanel.textContent += ` Your score: ${score}/5 - You lost!`;
     }
-
      // Remove buttons and stop graying out play button
+
      score = 0;
      round = 0;
+
+     const buttons = document.querySelector(".buttons");
+     for (let i = 0; i < 3; i++){
+        buttons.removeChild(buttons.lastChild);
+     }
 }
-
-
-const body = document.querySelector("body");
-const resultsPanel = document.querySelector(".resultsPanel");
-body.appendChild(resultsPanel);
-
-let score = 0;
-let round = 0;
 
 function game(){
 
+    
     // Pop up buttons for rock paper scissors
     const buttons = document.querySelector(".buttons");
+
     const rockButton = document.createElement('button');
+    rockButton.classList.toggle("rockButton");
     rockButton.textContent = "rock"
     rockButton.setAttribute("title", "co mam na czole co mam na czole");
     buttons.appendChild(rockButton);
@@ -92,22 +84,27 @@ function game(){
     rockButton.myParam = "rock";
 
     const paperButton = document.createElement('button');
+    rockButton.classList.toggle("paperButton");
     paperButton.textContent = "paper"
     buttons.appendChild(paperButton);
     paperButton.addEventListener("click", gameRound);
     paperButton.myParam = "paper";
 
     const scissorsButton = document.createElement('button');
+    rockButton.classList.toggle("scissorsButton");
     scissorsButton.textContent = "scissors"
     buttons.appendChild(scissorsButton);
     scissorsButton.addEventListener("click", gameRound);
     scissorsButton.myParam = "scissors";
-
-    //rock co mam na czole
-
-    // make game repeatable
 }
 
 
+const body = document.querySelector("body");
+const resultsPanel = document.querySelector(".resultsPanel");
+body.appendChild(resultsPanel);
+
 const playButton = document.querySelector(".playButton");
 playButton.addEventListener("click", game);
+
+let score = 0;
+let round = 0;
